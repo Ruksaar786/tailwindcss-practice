37c8b4171a1487e2e507ae0bfbdba4e614ac66f6
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-04-05 11:40:39 +0200

    ch3

---
+++ tailwind-css-practice/app/ch3/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ch3 = () => {
+  return (
+    <>
+      <header className="bg-[#23424a] text-white py-12">
+        <div className="w-[80%] mx-auto p-5">
+          <div className="text-white w-1/2">
+            <h1 className="text-4xl font-semibold mb-5">
+              Responsive design don&apos;t have to be a struggle
+            </h1>
+            <p className="text-lg mb-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+              aspernatur inventore necessitatibus molestias officiis cumque
+              dicta impedit voluptatum adipisci ad, corporis distinctio
+              excepturi esse est blanditiis libero labore veniam dolorum!
+            </p>
+            <button className="px-3 py-2 bg-purple-600 rounded-full uppercase">
+              I want to learn
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default ch3;
