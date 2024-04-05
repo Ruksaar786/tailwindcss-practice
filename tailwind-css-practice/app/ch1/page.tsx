264f543b2a88e99b8c70d657d19aaafcab762e94
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-04-05 10:51:55 +0200

    installed nextjs and did ch1 1

---
+++ tailwind-css-practice/app/ch1/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ch1 = () => {
+  return (
+    <div className="bg-teal-800 text-white p-10  ">
+      <div className="w-[50%]">
+        <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+          enim, tempore officia numquam consectetur nostrum fuga nesciunt
+          placeat dolores dolorem explicabo fugit voluptates doloribus eveniet
+          minus a. Inventore, in explicabo.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ch1;
