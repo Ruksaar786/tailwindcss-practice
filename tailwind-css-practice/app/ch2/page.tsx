1ea2217f950caf98d370a4beae27b6aa6cc18233
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-04-05 11:16:15 +0200

    ch2

---
+++ tailwind-css-practice/app/ch2/page.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const ch2 = () => {
+  return (
+    <>
+      <div className="bg-teal-800 text-white py-10  ">
+        <div className="w-[80%] mx-auto">
+          <div className="w-[50%]">
+            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+              enim, tempore officia numquam consectetur nostrum fuga nesciunt
+              placeat dolores dolorem explicabo fugit voluptates doloribus
+              eveniet minus a. Inventore, in explicabo.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="py-6">
+        <div className="w-[80%] mx-auto">
+          <div className="w-[70%]">
+            <h2 className="text-3xl mb-6">Lorem ipsum dolor sit amet.</h2>
+            <p className="mb-2">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              quia natus nostrum modi officia? Maxime aperiam natus unde sint
+              repellendus vitae recusandae excepturi deleniti consectetur modi.
+              Non repudiandae magnam rem!
+            </p>
+            <p className="mb-2">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint
+              nisi libero voluptates. Non hic beatae qui corporis porro odio
+              sequi eius fugiat alias quasi enim maxime molestiae, cumque nihil
+              adipisci?
+            </p>
+            <p className="mb-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+              exercitationem eos veniam maiores sed, modi quidem nisi recusandae
+              dolores laudantium esse consectetur, id sunt voluptatem ipsam quia
+              ea officiis facere.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ch2;
