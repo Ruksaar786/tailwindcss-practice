9532e02c4402b88046e048ef250c0a473c8867dd
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-04-12 10:38:26 +0200

    challenge 4

---
+++ tailwind-css-practice/app/ch4/page.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+const ch4 = () => {
+  return (
+    <>
+      <header className="bg-[#23424a] text-white py-12">
+        <div className="w-[80%] mx-auto p-5">
+          <div className="text-white w-1/2">
+            <h1 className="text-4xl font-semibold mb-5">
+              Responsive design don&apos;t have to be a struggle
+            </h1>
+            <p className="text-lg mb-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+              aspernatur inventore necessitatibus molestias officiis cumque
+              dicta impedit voluptatum adipisci ad, corporis distinctio
+              excepturi esse est blanditiis libero labore veniam dolorum!
+            </p>
+            <button className="px-3 py-2 bg-purple-600 rounded-full uppercase">
+              I want to learn
+            </button>
+          </div>
+        </div>
+      </header>
+
+      <section className=" gap-5 py-10 flex flex-row">
+        <div>
+          <h1 className="text-center text-3xl text-purple-800 font-semibold">
+            cheap
+          </h1>
+          <p className="text-xl">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
+            unde architecto iure iste voluptatibus, eaque quo molestias nulla
+            amet facere voluptatum aperiam non voluptates! Sint, repellat? Vitae
+            aut dolorum odio!
+          </p>
+        </div>
+        <div>
+          <h1 className="text-center text-3xl text-purple-800 font-semibold">
+            Quick
+          </h1>
+          <p className="text-xl">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint
+            debitis magni architecto doloremque incidunt voluptatem fuga
+            accusantium consectetur dignissimos ratione quas ab asperiores
+            consequatur atque perspiciatis cum voluptatibus, excepturi aliquid!
+          </p>
+        </div>
+        <div>
+          <h1 className="text-center text-3xl text-purple-800 font-semibold">
+            Affordable
+          </h1>
+          <p className="text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ut
+            incidunt omnis voluptate dolorum inventore perferendis aspernatur
+            aperiam? Quaerat aut quisquam cupiditate quis, quidem repellendus
+            optio nulla vitae ratione maxime?
+          </p>
+        </div>
+      </section>
+
+      <section className="flex flex-row w-[80%] mx-auto bg-teal-900 text-white">
+        <div className="gap-10">
+          <h1 className="text-center font-semibold mb-5">cheap</h1>
+          <p className=" text-lg ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+            quibusdam voluptas, tempore suscipit nulla explicabo omnis, quis
+            repellendus laudantium ducimus enim odit earum recusandae aperiam
+            placeat adipisci quisquam iste officia.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-center mb-5">Quick</h2>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nihil
+            asperiores doloremque unde odio veniam maxime esse dolorum illo
+            autem ratione quisquam id deleniti, aliquid quod mollitia rerum
+            soluta. Porro?
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ch4;
